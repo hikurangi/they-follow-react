9b1142168ee90b5eb59ff2b608967ffb21d00553
hikurangi <hikurangi@gmail.com>
2016-09-28 16:07:43 +1300

    css trouble

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 import mapboxgl from 'mapbox-gl'
+var fs = require('fs');
+require('dotenv').config()
 
 console.log('welcome to they-follow')
+
+// var locator = document.getElementById("locator");
+// function getLocation() {
+//     if (navigator.geolocation) {
+//         navigator.geolocation.getCurrentPosition(showPosition);
+//     } else {
+//         locator.innerHTML = "Geolocation is not supported by this browser.";
+//     }
+// }
+// function showPosition(position) {
+//     locator.innerHTML = "Latitude: " + position.coords.latitude +
+//     "<br>Longitude: " + position.coords.longitude;
+// }
 
 // mapboxgl.accessToken = process.env.MAPBOX_GL_KEY
 mapboxgl.accessToken = 'pk.eyJ1IjoiaGlrdXJhbmdpIiwiYSI6ImNpdG1hYmI2YzAweHgyeG4wYXE5MGhmeTIifQ.L9Kkv_kbglN626HmG6ryfQ'
